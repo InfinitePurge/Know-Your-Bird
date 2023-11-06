d0bd34851dc9b8dbc92e6285c7340495ecbdd0df
MuCHai <edvinasbabilas@gmail.com.com>
2023-11-06 19:22:11 +0200

    new file:   public/images/alus.png 	new file:   public/images/questionmark.png 	modified:   resources/js/components/ExampleComponent.vue 	modified:   resources/views/welcome.blade.php

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,52 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="carousel">
+    <transition name="fade" mode="out-in">
+      <img :key="currentImage" :src="images[currentImage]" alt="Image" class="carousel-image" />
+    </transition>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  data() {
+    return {
+      images: [
+        "{{ asset('photos/atsisiųsti.jpg') }}",  // Updated image path
+        "{{ asset('photos/image1.jpg') }}",
+        "{{ asset('photos/image2.jpg') }}",
+        "{{ asset('photos/image3.jpg') }}",
+        // Add more image paths here
+      ],
+      currentImage: 0,
+    };
+  },
+  mounted() {
+    this.startCarousel();
+  },
+  methods: {
+    startCarousel() {
+      setInterval(() => {
+        this.currentImage = (this.currentImage + 1) % this.images.length;
+      }, 3000);
+    },
+  },
+};
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.carousel {
+  width: 100%; /* Make the carousel container responsive */
+}
+
+.carousel-image {
+  width: 100%; /* Make the image fluid horizontally */
+  height: auto;
+}
+</style>
