186b086b860bfa24f88a26e22549aa2cc1d5a0d3
InfinitePurge <edvinasbabilas@gmail.com>
2023-11-23 16:11:02 +0200

    Veikiantis filteris nebelyskit

--- app/Http/Controllers/BirdsListController.php
+++ app/Http/Controllers/BirdsListController.php
@@ -8,27 +8,11 @@ use Illuminate\Support\Facades\Log;
 
 class BirdsListController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $birdsQuery = Pauksciai::query();
-
-        // Check if a continent is selected
-        if ($request->has('continent')) {
-            $selectedContinent = $request->input('continent');
-            $birdsQuery->where('kilme', $selectedContinent);
-        }
-
-        // Log the SQL query to see what's being executed
-        Log::info($birdsQuery->toSql());
-
-        // Paginate the results
-        $birds = $birdsQuery->paginate(15);
-
-        if ($request->ajax()) {
-            return view('partials.bird_cards', compact('birds'));
-        }
-
-        return view('birdlist', compact('birds'));
+        $birds = Pauksciai::all();
+        $kilmeValues = Pauksciai::select('kilme')->distinct()->pluck('kilme');
+        return view('birdlist', compact('birds', 'kilmeValues'));
     }
 
     public function view($pavadinimas)
@@ -45,12 +29,5 @@ class BirdsListController extends Controller
 
         // Pass bird data to the view
         return view('bird', compact('bird'));
-    }
-
-
-    public function fetchContinents()
-    {
-        $continents = Pauksciai::select('kilme')->distinct()->pluck('kilme');
-        return response()->json($continents);
     }
 }
---
+++ public/jasonas/filtras.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // Attach a click event listener to the kilme dropdown items
+    $('.DropDownText').on('click', function () {
+        // Get the selected kilme value
+        var selectedKilme = $(this).text();
+
+        console.log("Selected Kilme:", selectedKilme);
+
+        // Hide all bird cards
+        $('.bird-card').hide();
+
+        // Show only the cards with the selected kilme
+        $('.bird-card[data-continent="' + selectedKilme + '"]').show();
+
+        // Log the number of cards found
+        var numCards = $('.bird-card[data-continent="' + selectedKilme + '"]').length;
+        console.log("Number of cards with selected Kilme:", numCards);
+    });
+});
